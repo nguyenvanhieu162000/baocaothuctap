fc3ad0053899a2229238a21b3cc34a74ab7e22df
WINDOWS-10\Administrator <nguyenvanhieu162000@gmail.com>
2022-08-29 18:42:57 +0700

    ok

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { Filter, FilterButton } from 'src/app/modules/filtering.model';
+import { TodoService } from 'src/app/services/todo.service';
+import { map, takeUntil } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent implements OnInit, OnDestroy {
+  filterButtons: FilterButton[] = [
+    { type: Filter.All, label: 'All', isActive: true },
+    { type: Filter.Active, label: 'Active', isActive: false },
+    { type: Filter.Completed, label: 'Completed', isActive: false },
+  ];
+
+  length = 0;
+  hasComplete$: Observable<boolean>;
+  destroy$: Subject<void> = new Subject<void>();
+
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit() {
+    this.hasComplete$ = this.todoService.todos$.pipe(
+      map((todos) => todos.some((t) => t.isCompleted)),
+      takeUntil(this.destroy$)
+    );
+
+    this.todoService.length$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((length) => {
+        this.length = length;
+      });
+  }
+
+  filter(type: Filter) {
+    this.setActiveFilterBtn(type);
+    this.todoService.filterTodos(type);
+  }
+
+  private setActiveFilterBtn(type: Filter) {
+    this.filterButtons.forEach((btn) => {
+      btn.isActive = btn.type === type;
+    });
+  }
+
+  clearCompleted(){
+    this.todoService.clearCompleted();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
